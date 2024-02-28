7a5f967a0979dacb116ada228f290133298f1b20
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-28 12:59:29 +0100

    cambio session por server como variable global

--- nivel1ejercicio1/formulario.php
+++ nivel1ejercicio1/formulario.php
@@ -1,14 +1,13 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    
+    echo "<h2>Datos recibidos!</h2>";
+    echo "El nombre es: " . $_SESSION["nombre"] . "</br>";
+    echo "El correo es: " . $_SESSION["correo"];
+}
 
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-
-$_SESSION["nombre"] = $nombre;
-$_SESSION["correo"] = $correo;
-
-
-echo "<h2>Datos recibidos!</h2>";
-echo "El nombre es: " . $_SESSION["nombre"] . "</br>";
-echo "El correo es: " . $_SESSION["correo"];
-
+//$_SESSION["nombre"] = $nombre;
+//$_SESSION["correo"] = $correo;
 ?>
