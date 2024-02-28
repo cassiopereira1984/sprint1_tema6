0d5b7659b2969be583246e087a34975659aa5a64
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-28 12:30:34 +0100

    finish

--- nivel1ejercicio1/formulario.php
+++ nivel1ejercicio1/formulario.php
@@ -1,8 +1,14 @@
 <?php
+
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
 
+$_SESSION["nombre"] = $nombre;
+$_SESSION["correo"] = $correo;
+
+
 echo "<h2>Datos recibidos!</h2>";
-echo "El nombre es: " . $nombre . "\n";
-echo "El correo es: " . $correo . "\n";
+echo "El nombre es: " . $_SESSION["nombre"] . "</br>";
+echo "El correo es: " . $_SESSION["correo"];
+
 ?>
