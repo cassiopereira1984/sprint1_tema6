1f0357bf810bf2f95c913553d7411e9c071aedbc
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-28 10:56:54 +0100

    +.php + tag from + method post

---
+++ nivel1ejercicio1/formulario.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+
+echo "<h2>Datos recibidos!</h2>";
+echo "El nombre es: " . $nombre . "\n";
+echo "El correo es: " . $correo . "\n";
+?>
